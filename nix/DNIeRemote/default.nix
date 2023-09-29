42541e5a15104639444fcf253446da8eec315d04
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-09-29 23:04:05 +0200

    fix: add adb for USB connection

--- nix/DNIeRemote/default.nix
+++ nix/DNIeRemote/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchzip, dpkg, autoPatchelfHook, gtkmm3, atkmm, glibmm, gtk3, pango, at-spi2-atk, cairo, openssl_1_1 }:
+{ lib, stdenv, fetchzip, dpkg, autoPatchelfHook, gtkmm3, atkmm, glibmm, gtk3, pango, at-spi2-atk, cairo, openssl_1_1, android-tools, makeWrapper }:
 stdenv.mkDerivation rec {
   name = "DNIeRemote";
   version = "1.0-5";
@@ -17,9 +17,13 @@ stdenv.mkDerivation rec {
     at-spi2-atk
     cairo
     openssl_1_1
+    makeWrapper
   ];
   nativeBuildInputs = [ 
     autoPatchelfHook
+  ];
+  propagatedBuildInputs = [
+    android-tools
   ];
   unpackCmd = "dpkg-deb -x $src/DNIeRemoteSetup_${version}_amd64.deb .";
   installPhase = ''
@@ -28,4 +32,18 @@ stdenv.mkDerivation rec {
     cp -r bin local/lib share $out
     runHook postInstall
   '';
+  postFixup = ''
+    wrapProgram $out/bin/dnieremotewizard \
+      --set PATH ${lib.makeBinPath [
+        android-tools
+      ]}
+  '';
+  meta = with lib; {
+    description = "Enables NFC-based DNIe 3.0 reading for PCs, offering digital authentication for Spain's electronic administration services";
+    homepage = "https://www.dnielectronico.es/PortalDNIe/PRF1_Cons02.action?pag=REF_1015&id_menu=65";
+    license = with licenses; [ ];  # TODO: find out
+    maintainers = with maintainers; [ nilp0inter ];
+    mainProgram = "dnieremotewizard";
+    platforms = platforms.linux;
+  };
 }

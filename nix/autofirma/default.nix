d81905705998ab92d9e1bed7ba4675c0624b3e85
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-09-30 23:21:13 +0200

    feature: compile all afirma dependencies and load firefox NSS cert store

--- nix/autofirma/default.nix
+++ nix/autofirma/default.nix
@@ -6,17 +6,54 @@
   makeWrapper,
   maven,
   nss,
+  firefox,
   runtimeShell,
   JAVAX_NET_SSL_TRUSTSTORE ? null,
 }: let
   pname = "autofirma";
   version = "1.8.2";
-  source = {
+  jmulticard-src = stdenv.mkDerivation {
+    name = "jmulticard-src";
     src = fetchFromGitHub {
+      owner = "ctt-gob-es";
+      repo = "jmulticard";
+      rev = "v1.8";
+      hash = "sha256-sCqMK4FvwRHsGIB6iQVyqrx0+EDiUfQSAsPqmDq2Giw=";
+    };
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/
+      cp -R . $out/
+    '';
+    dontFixup = true;
+  };
+  clienteafirma-external-src = stdenv.mkDerivation {
+    name = "clienteafirma-external-src";
+    src = fetchFromGitHub {
+      name = "clienteafirma-external";
+      owner = "ctt-gob-es";
+      repo = "clienteafirma-external";
+      rev = "v1.0.5";
+      hash = "sha256-cCH4iOvtAZ+9VsHG+MrfU5DkJ+cvXZvDTRDIamuBZZw=";
+    };
+    patches = [
+      ./patches/clienteafirma-external/javaversion.patch
+    ];
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/
+      cp -R . $out/
+      '';
+    dontFixup = true;
+  };
+  clienteafirma-src = stdenv.mkDerivation {
+    name = "clienteafirma-src";
+    src = fetchFromGitHub {
+      name = "clienteafirma";
       owner = "ctt-gob-es";
       repo = "clienteafirma";
       rev = "v${version}";
-      sha256 = "sha256-YtGtTeWDWwCCIikxs6Cyrypb0EBX2Q2sa3CBCmC6kK8=";
+      hash = "sha256-YtGtTeWDWwCCIikxs6Cyrypb0EBX2Q2sa3CBCmC6kK8=";
     };
     patches = [
       ./patches/javaversion.patch
@@ -24,13 +61,28 @@
       ./patches/nsspath.patch
       ./patches/pom.patch
     ];
+    dontBuild = true;
+    installPhase = ''
+      mkdir -p $out/
+      cp -R . $out/
+    '';
     postPatch = ''
       substituteInPlace afirma-keystores-mozilla/src/main/java/es/gob/afirma/keystores/mozilla/MozillaKeyStoreUtilitiesUnix.java \
-        --replace '@nsspath' '${nss}/lib'
+        --replace '@nsspath' '${nss}/lib' \
+        --replace '@firefoxpath' '${firefox}/lib/firefox'
 
       substituteInPlace afirma-ui-simple-configurator/src/main/java/es/gob/afirma/standalone/configurator/ConfiguratorFirefoxLinux.java \
         --replace '@certutilpath' '${nss.tools}/bin/certutil'
     '';
+    dontFixup = true;
+  };
+  source = {
+    srcs = [
+      jmulticard-src
+      clienteafirma-external-src
+      clienteafirma-src
+    ];
+    sourceRoot = clienteafirma-src.name;
   };
 
   afirma-libs = stdenv.mkDerivation (source
@@ -44,6 +96,13 @@
       buildPhase = ''
         runHook preBuild
 
+        cd ${jmulticard-src}
+        mvn clean dependency:go-offline -Dmaven.repo.local=$out/.m2
+
+        cd ${clienteafirma-external-src}
+        mvn clean dependency:go-offline -Dmaven.repo.local=$out/.m2
+
+        cd ${clienteafirma-src}
         mvn clean dependency:go-offline -Dmaven.repo.local=$out/.m2 -Denv=dev
 
         # The following command downloads all dependencies from mavencentral, including the ones that
@@ -53,7 +112,8 @@
         # So we need to remove them from the local repository, while keeping
         # es/gob/afirma/lib which contains some dependencies that are not in this
         # repository.
-        rm -Rf $out/.m2/es/gob/afirma/afirma-*
+        # rm -Rf $out/.m2/es/gob/afirma/afirma-*
+        rm -Rf $out/.m2/es/gob/afirma
 
         runHook postBuild
       '';
@@ -73,7 +133,7 @@
       dontFixup = true;
       outputHashAlgo = "sha256";
       outputHashMode = "recursive";
-      outputHash = "sha256-WShdCwuUEFUBQqayfkXUjvGjF+nswVySN2kAzxittmM=";
+      outputHash = "sha256-m23F+jZV9P3jzDdnzk2luI9nQMxu1VKotSPJzShr1ss=";
     });
 in
   stdenv.mkDerivation (source
@@ -89,6 +149,23 @@ in
         runHook preBuild
 
         mvnDeps=$(cp -dpR ${afirma-libs}/.m2 ./ && chmod +w -R .m2 && pwd)
+
+        cp -dpR ${jmulticard-src} ./jmulticard
+        chmod +w -R jmulticard
+        cd jmulticard
+        mvn clean install -o -nsu "-Dmaven.repo.local=$mvnDeps/.m2" -DskipTests
+        cd ..
+
+
+        cp -dpR ${clienteafirma-external-src} ./clienteafirma-external
+        chmod +w -R clienteafirma-external
+        cd clienteafirma-external
+        mvn clean install -o -nsu "-Dmaven.repo.local=$mvnDeps/.m2" -DskipTests
+        cd ..
+
+        cp -dpR ${clienteafirma-src} ./clienteafirma
+        chmod +w -R clienteafirma
+        cd clienteafirma
         mvn clean install -o -nsu "-Dmaven.repo.local=$mvnDeps/.m2" -Denv=dev -DskipTests
         mvn clean package -o -nsu "-Dmaven.repo.local=$mvnDeps/.m2" -Denv=install -DskipTests
 
@@ -112,6 +189,7 @@ in
           --replace /usr/bin/autofirma $out/bin/autofirma
 
         makeWrapper ${jre}/bin/java $out/bin/autofirma \
+          --add-flags "-Des.gob.afirma.keystores.mozilla.UseEnvironmentVariables=true" \
           --add-flags "-jar $out/lib/AutoFirma/AutoFirma.jar" \
           ${lib.optionalString (JAVAX_NET_SSL_TRUSTSTORE != null) "--set JAVAX_NET_SSL_TRUSTSTORE '${JAVAX_NET_SSL_TRUSTSTORE}'"}
 

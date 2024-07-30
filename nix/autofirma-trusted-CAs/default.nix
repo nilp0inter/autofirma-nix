89899c9e15be71cd5fb9b91f76a40eeb2f747cd7
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2024-07-30 21:52:09 +0200

    feat: a tool to download CAs from urls

---
+++ nix/autofirma-trusted-CAs/default.nix
@@ -0,0 +1,11 @@
+{fetchurl}: let
+  trusted-providers = builtins.fromJSON (builtins.readFile ./providers.json);
+  read-provider-certs = provider: map (cert: {inherit provider cert;}) (builtins.fromJSON (builtins.readFile ./CAs-by-provider/${provider.cif}.json));
+  fetch-ca = trusted:
+    fetchurl {
+      url = trusted.cert.url;
+      sha256 = trusted.cert.hash;
+      meta = {inherit trusted;};
+    };
+in
+  map fetch-ca (builtins.concatMap read-provider-certs trusted-providers)

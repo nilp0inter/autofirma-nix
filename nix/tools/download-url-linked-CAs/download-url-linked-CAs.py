bfb4a80dc1888947ee8fd4dd01090fa014a2b180
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2024-07-30 22:47:39 +0200

    feat: add concurrent processing of certificate URLs

--- nix/tools/download-url-linked-CAs/download-url-linked-CAs.py
+++ nix/tools/download-url-linked-CAs/download-url-linked-CAs.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python3
+
+from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urljoin
+import argparse
+import json
 import os
-import sys
 import re
 import subprocess
-import json
-from urllib.parse import urljoin
+import sys
+
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
+
 
 def get_urls(url, driver):
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     return [urljoin(url, a['href']) for a in soup.find_all('a', href=re.compile(r'\.(crt|cert?|pem)$'))]
 
+
 def is_self_signed(path):
     result = subprocess.run(['openssl', 'x509', '-in', path, '-noout', '-issuer', '-subject'], 
                             capture_output=True, text=True)
-    issuer_subject = set(line.split('=', 1)[1] for line in result.stdout.strip().split('\n'))
+    try:
+        issuer_subject = set(line.split('=', 1)[1] for line in result.stdout.strip().split('\n'))
+    except IndexError:
+        print(f"Error processing {path}: {result.stderr}", file=sys.stderr)
+        return None
     return len(issuer_subject) == 1
+
 
 def get_sri_hash(path):
     flat_hash = subprocess.run(['nix-hash', '--type', 'sha256', '--flat', path], 
@@ -28,33 +39,53 @@ def get_sri_hash(path):
     return subprocess.run(['nix-hash', '--type', 'sha256', '--to-sri', flat_hash], 
                           capture_output=True, text=True).stdout.strip()
 
-def main(url):
+
+def process_cert_url(cert_url):
+    print(f"Processing {cert_url}", file=sys.stderr)
+    result = subprocess.run(['nix-prefetch-url', '--type', 'sha256', '--print-path', cert_url], 
+                            capture_output=True, text=True)
+
+    match = re.search(r'^/.*', result.stdout, re.MULTILINE)
+    if not match:
+        print(f"Error processing {cert_url}: {result.stderr}", file=sys.stderr)
+        return None
+    else:
+        path = match.group(0)
+
+        if is_self_signed(path):
+            hash_value = get_sri_hash(path)
+            return {'url': cert_url, 'hash': hash_value}
+
+        return None  # Not self-signed
+
+
+def main(args):
     # Set up Selenium WebDriver
     options = Options()
-    options.add_argument('--headless=new')
+    if args.headless:
+        options.add_argument('--headless=new')
+    options.add_argument('--disable-application-cache')
+    options.add_argument(f'--user-agent={args.user_agent}')
     service = Service(os.environ.get('CHROMEDRIVER_PATH'))
     driver = webdriver.Chrome(service=service, options=options)
+    driver.execute_cdp_cmd('Network.setCacheDisabled', {'cacheDisabled': True})
     
     results = []
-    try:
-        for cert_url in get_urls(url, driver):
-            try:
-                result = subprocess.run(['nix-prefetch-url', '--type', 'sha256', '--print-path', cert_url], 
-                                        capture_output=True, text=True)
-                path = re.search(r'^/.*', result.stdout, re.MULTILINE).group(0)
-                
-                if is_self_signed(path):
-                    hash_value = get_sri_hash(path)
-                    results.append({'url': cert_url, 'hash': hash_value})
-            except Exception as e:
-                print(f"Error processing {cert_url}: {e}", file=sys.stderr)
-    finally:
-        driver.quit()
+    with ThreadPoolExecutor(max_workers=args.max_workers) as executor:
+        try:
+            results = list(filter(None, executor.map(process_cert_url, get_urls(args.url, driver))))
+        finally:
+            driver.quit()
     
     print(json.dumps(results, indent=2))
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-    main(sys.argv[1])
+    parser = argparse.ArgumentParser(description='Download and hash certificates linked from a URL')
+    parser.add_argument('url', help='URL to scrape for certificates')
+    parser.add_argument('--headless', action=argparse.BooleanOptionalAction, help='Run Chrome in headless mode', default=True)
+    parser.add_argument('-n', '--max-workers', type=int, default=8, help='Maximum number of workers')
+    parser.add_argument('--user-agent', help='User agent string to use', default='Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0')
+
+    args = parser.parse_args()
+
+    main(args)
